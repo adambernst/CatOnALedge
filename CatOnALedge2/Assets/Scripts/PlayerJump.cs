8fe4b2c7c152cadb954afc1a0c122f6c1d24bb5d
adambernst <adam.bernstein@tufts.edu>
2022-03-29 17:01:08 -0400

    Copied Previous Project with new Materials

---
+++ CatOnALedge2/Assets/Scripts/PlayerJump.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[RequireComponent(typeof(Rigidbody))]
+
+public class PlayerJump : MonoBehaviour {
+    public Vector3 jump;
+    public float jumpForce = 2.0f;
+
+    public bool isGrounded;
+    Rigidbody rb;
+    void Start(){
+        rb = GetComponent<Rigidbody>();
+        jump = new Vector3(0.0f, 2.0f, 0.0f);
+    }
+
+    void OnCollisionStay(){
+        isGrounded = true;
+    }
+
+    void Update(){
+        if(Input.GetButtonDown("Jump") && isGrounded){
+
+            rb.AddForce(jump * jumpForce, ForceMode.Impulse);
+            isGrounded = false;
+        }
+    }
+}
---
+++ CatOnALedge2/Assets/Scripts/PlayerMove3D.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove3D : MonoBehaviour {
+    //Animator anim;
+    Rigidbody rb;
+    public Transform cam;
+
+    public float speed = 6f;
+    public float turnSmoothTime =0.1f;
+    private float turnSmoothVelocity;
+
+    void Start(){
+        //anim = gameObject.GetComponentInChildren<Animator>();
+        rb = gameObject.GetComponent<Rigidbody>();
+    }
+
+    void Update () {
+        float horiz = Input.GetAxisRaw("Horizontal");
+        float vert = Input.GetAxisRaw("Vertical");
+        Vector3 direct = new Vector3(horiz, 0f, vert).normalized;
+
+        if (direct.magnitude >= 0.1f) {
+            float targetAngle = Mathf.Atan2(direct.x, direct.z) * Mathf.Rad2Deg + cam.eulerAngles.y;
+            float angle = Mathf.SmoothDampAngle(transform.eulerAngles.y, targetAngle, ref turnSmoothVelocity, turnSmoothTime);
+            transform.rotation = Quaternion.Euler(0f, angle, 0f);
+            Vector3 moveDir = Quaternion.Euler(0f, targetAngle, 0f) * Vector3.forward;
+            rb.MovePosition(transform.position + moveDir * speed * Time.deltaTime);
+        }
+    }
+}
