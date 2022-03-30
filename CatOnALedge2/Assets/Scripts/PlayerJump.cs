4f67c49e4d1200ca4c188be6967e1ba032a748d4
adambernst <adam.bernstein@tufts.edu>
2022-03-30 16:40:03 -0400

    Working on jump, it doesn't work anymore though rip

--- CatOnALedge2/Assets/Scripts/PlayerJump.cs
+++ CatOnALedge2/Assets/Scripts/PlayerJump.cs
@@ -5,25 +5,37 @@ using System.Collections.Generic;
 [RequireComponent(typeof(Rigidbody))]
 
 public class PlayerJump : MonoBehaviour {
-    public Vector3 jump;
+    
     public float jumpForce = 2.0f;
-
+    public float fallMultiplier = 2.5f;
+    public Vector3 jump;
+    
     public bool isGrounded;
+    
     Rigidbody rb;
-    void Start(){
+    
+    void Awake() {
         rb = GetComponent<Rigidbody>();
         jump = new Vector3(0.0f, 2.0f, 0.0f);
     }
-
-    void OnCollisionStay(){
-        isGrounded = true;
+    
+    private void OnCollisionStay(Collision other){
+        if (other.collider.CompareTag("Ground"))
+            isGrounded = true;
     }
-
-    void Update(){
-        if(Input.GetButtonDown("Jump") && isGrounded){
-
+    
+    void Update() {
+        // if (rb.velocity.y < 0) {
+        if (!isGrounded) {
+            rb.velocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.deltaTime;
+        }
+        // else if (rb.velocity.y > 0 && (Input.GetButton("Jump"))) 
+        else if (isGrounded && (Input.GetButton("Jump"))) {
+            // rb.velocity += Vector3.up * Physics.gravity.y * (jumpForce - 1) * Time.deltaTime;
             rb.AddForce(jump * jumpForce, ForceMode.Impulse);
             isGrounded = false;
         }
+        
+        
     }
 }
