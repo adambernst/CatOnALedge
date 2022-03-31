52459b8bec2953028edf4b01b22dfb9065da0e89
adambernst <adam.bernstein@tufts.edu>
2022-03-30 21:56:12 -0400

    More jump work, still doesn't work :(

--- CatOnALedge2/Assets/Scripts/PlayerJump.cs
+++ CatOnALedge2/Assets/Scripts/PlayerJump.cs
@@ -2,42 +2,62 @@ using System.Collections.Generic;
 using System.Collections;
 using UnityEngine;
 
-public class PlayerJump : MonoBehaviour {
+[RequireComponent(typeof(CharacterController))]
 
-     public float speed;
-     public float rotSpeed;
-     public float JumpHeight;
-     public bool isGrounded;
-     public float GravityStrength;
-     Rigidbody rb;
-     
+public class PlayerJump : MonoBehaviour {
+    
+    // private CharacterController characterController;
+
+    // public float speed;
+    // public float rotSpeed;
+    public float jumpHeight;
+    // public Vector3 velocity;
+    private bool isGrounded;
+    // private bool jumpPressed = false;
+    // public float gravityStrength = -9.81;
+    Rigidbody rb;
+     // 
      void Start() {
-         Vector3 gravityS = new Vector3(0, GravityStrength, 0);
+         // characterController = GetComponent<CharacterController>();
          
-         Physics.gravity = gravityS;
+         
+         // Vector3 gravityS = new Vector3(0, GravityStrength, 0);
+         // 
+         // Physics.gravity = gravityS;
          rb = GetComponent<Rigidbody>();
-         isGrounded = true;
+         // isGrounded = true;
      }
      
      void Update() {
-         var z = Input.GetAxis("Vertical") * speed;
-         var y = Input.GetAxis("Horizontal") * rotSpeed;
-         
-         transform.Translate(0, 0, z);
-         transform.Rotate(0, y, 0);
-         
-         if (Input.GetKey(KeyCode.Space) && isGrounded == true) {
+         if (Input.GetButtonDown("Jump") && isGrounded) {
              Jump();
          }
-     }
-     
+         
+            
+     //     var z = Input.GetAxis("Vertical") * speed;
+     //     var y = Input.GetAxis("Horizontal") * rotSpeed;
+     // 
+     //     transform.Translate(0, 0, z);
+     //     transform.Rotate(0, y, 0);
+     // 
+     //     if (Input.GetKey(KeyCode.Space) && isGrounded == true) {
+     //         Jump();
+        // }
+    }
+    
+     // 
      void Jump() {
-         rb.AddForce(new Vector3(0, JumpHeight, 0));
+         Debug.Log("Jump pressed");
+         
+         
+         
+         
+         rb.AddForce(new Vector3(0, jumpHeight, 0));
          isGrounded = false;
          // Animation goes here
      }
      
-     void OnCollisionEnter(Collision other) {
+     void OnCollisionStay(Collision other) {
          // if (other.collisionInfo.Collider.tag == "Ground")
             isGrounded = true;
      }
