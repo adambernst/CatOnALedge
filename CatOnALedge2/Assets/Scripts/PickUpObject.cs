f6e785b74fc60d8a596e43d5dc34b32f8241afa3
TristanMartello <64500848+TristanMartello@users.noreply.github.com>
2022-04-17 20:51:45 -0400

    better messages and some script organizing

--- CatOnALedge2/Assets/Scripts/PickUpObject.cs
+++ CatOnALedge2/Assets/Scripts/PickUpObject.cs
@@ -22,9 +22,9 @@ public class PickUpObject : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(canpickup == true) // if you enter thecollider of the objecct
+        if(canpickup) // if you enter thecollider of the objecct
         {
-            if (Input.GetKey("e") && hasItem == false)  // can be e or any key
+            if (Input.GetKey("e") && !hasItem)  // can be e or any key
             {
                 displayMessage = false;
                 kitten.GetComponent<Rigidbody>().isKinematic = true;   //makes the rigidbody not be acted upon by forces
@@ -33,7 +33,7 @@ public class PickUpObject : MonoBehaviour
                 hasItem = true;
             }
         }
-        if (Input.GetKey("q") && hasItem == true) // if you have an item and get the key to remove the object, again can be any key
+        if (Input.GetKey("q") && hasItem) // if you have an item and get the key to remove the object, again can be any key
         {
             kitten.GetComponent<Rigidbody>().isKinematic = false; // make the rigidbody work again
             Vector3 modifier = new Vector3(0f, -2.1f, 0f);
@@ -44,7 +44,7 @@ public class PickUpObject : MonoBehaviour
     }
     private void OnTriggerEnter(Collider other) // to see when the player enters the collider
     {
-        if(other.gameObject.tag == "kitten") //on the object you want to pick up set the tag to be anything, in this case "object"
+        if(other.gameObject.tag == "kitten" && !hasItem) //on the object you want to pick up set the tag to be anything, in this case "object"
         {
             canpickup = true;  //set the pick up bool to true
             kitten = other.gameObject; //set the gameobject you collided with to one you can reference
@@ -59,7 +59,7 @@ public class PickUpObject : MonoBehaviour
     
     void OnGUI()
     {
-        if (displayMessage) {
+        if (displayMessage && !hasItem) {
             GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 200f, 200f), message);
         }
     }
