f6312bd089c7b64b5cc4715160faacf9439d9f51
TristanMartello <64500848+TristanMartello@users.noreply.github.com>
2022-04-20 13:29:29 -0400

    fixed some kitten bugs

--- CatOnALedge2/Assets/Scripts/PickUpObject.cs
+++ CatOnALedge2/Assets/Scripts/PickUpObject.cs
@@ -7,12 +7,12 @@ public class PickUpObject : MonoBehaviour
     public GameObject mouth; //reference to your hands/the position where you want your object to go
     bool canpickup; //a bool to see if you can or cant pick up the item
     private GameObject kitten; // the gameobject onwhich you collided with
-    private KittenManager kitScript;
+    //private KittenManager kitScript;
     bool hasItem; // a bool to see if you have an item in your hand
-    
-    public string message = "Press E to pick up the kitten and Q to put it down";
-    bool displayMessage = false;
     bool overBed = false;
+    
+    bool displayMessage = false;
+    int messageCode = 0;
         
     // Start is called before the first frame update
     void Start()
@@ -43,47 +43,36 @@ public class PickUpObject : MonoBehaviour
             kitten.transform.parent = null; // make the object not be a child of the hands
             hasItem = false;
             if (overBed){
-                //KittenManager kitScript = kitten.GetComponent<KittenManager>();
-                kitScript.isHome = true;
                 Destroy(kitten);
+                overBed = false;
             }
         }
     }
+    
     private void OnTriggerEnter(Collider other) // to see when the player enters the collider
     //private void OnCollisionEnter(Collision other)
     {
-        Debug.Log(other.gameObject.tag);
+        //Debug.Log(other.gameObject.tag);
         if(other.gameObject.tag == "kitten" && !hasItem) //on the object you want to pick up set the tag to be anything, in this case "object"
         {
-            //KittenManager manager = other.gameObject.GetComponent<KittenManager>();
-            //if (!manager.isHome)
-            if (!hasItem)
-            {
-                canpickup = true;  //set the pick up bool to true
-                kitten = other.gameObject; //set the gameobject you collided with to one you can reference
-                kitScript = kitten.transform.parent.gameObject.GetComponent<KittenManager>();
-                displayMessage = true;    
-            }
+            canpickup = true;  //set the pick up bool to true
+            kitten = other.gameObject; //set the gameobject you collided with to one you can reference
+            GameObject kitParent = kitten.transform.root.gameObject; //parent.gameObject;
+            //kitScript = kitParent.GetComponent<KittenManager>();
+            displayMessage = true;
+            messageCode = 1;
+            overBed = false;
         }
     }
     
     private void OnCollisionEnter(Collision other)
     {
-        //Debug.Log("weiner " + other.gameObject.tag);
         if(other.gameObject.tag == "kittenBed"){
             overBed = true;
         }
     }
-    private void OnCollisionExit(Collision other)
-    {
-        //Debug.Log("off weiner " + other.gameObject.tag);
-        if(other.gameObject.tag == "kittenBed"){
-            overBed = false;
-        }
-    }
-    
+
     private void OnTriggerExit(Collider other)
-    //private void OnCollisionExit(Collision other)
     {
         canpickup = false; //when you leave the collider set the canpickup bool to false
         displayMessage = false;
@@ -91,8 +80,12 @@ public class PickUpObject : MonoBehaviour
     
     void OnGUI()
     {
-        if (displayMessage && !hasItem) {
-            GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 200f, 200f), message);
+        string message = "";
+        if (messageCode == 1 && displayMessage && !hasItem) {
+            message = "Press E to pick up the kitten and Q to put it down";
+        } else if (messageCode == 2 && overBed) {
+            message = "Place the kittens in the cat bed to save them";
         }
+        GUI.Label(new Rect(Screen.width / 2, Screen.height / 2, 200f, 200f), message);
     }
 }
