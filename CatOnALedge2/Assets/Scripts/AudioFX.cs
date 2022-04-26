b1e29434ba13f7b6d2ca5a904d8b6cc5fe3c4ad5
adambernst <adam.bernstein@tufts.edu>
2022-04-26 16:28:42 -0400

    Working on audio

---
+++ CatOnALedge2/Assets/Scripts/AudioFX.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioFX : MonoBehaviour
+{
+    public AudioSource thud;
+    public AudioSource meow1;
+    public AudioSource meow2;
+    public AudioSource meow3;
+    
+    public void PlayThud() {
+        thud.Play();
+    }
+    
+    public void PlayMeow1() {
+        meow1.Play();
+    }
+    
+    
+    
+    
+    
+    
+    
+}
