6d54ee72d2f7872ec77c3164dd5744e720241da1
adambernst <adam.bernstein@tufts.edu>
2022-05-10 15:26:57 -0400

    Win scenes done!

--- CatOnALedge2/Assets/DisplayCount.cs
+++ CatOnALedge2/Assets/DisplayCount.cs
@@ -1,18 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class DisplayCount : MonoBehaviour
 {
+    
+    public GameObject returnedText;
+    
     // Start is called before the first frame update
     void Start()
     {
-        
+        Text textBox = returnedText.GetComponent<Text>();
+        textBox.text = "Kittens Returned: " + GameHandler.control.kittenCount;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
